2104f92c1a9e6ebd4d1769e63dc2864472a088bd
Scott Daniel <scottdaniel25@gmail.com>
2017-10-05 14:53:42 -0700

    another fing

--- scripts/workers/interactive_tcga_bioLinks.R
+++ scripts/workers/interactive_tcga_bioLinks.R
@@ -1,9 +1,11 @@
 # source("https://bioconductor.org/biocLite.R")
 # biocLite("TCGAbiolinks")
 
-library("TCGAbiolinks")
+library(TCGAbiolinks)
+library(dplyr)
+library(DT)
 
-query <- GDCquery(project = "TCGA-READ",
-                 data.category = "Transcriptome Profiling",
-                 data.type = "Gene Expression Quantification",
-                 workflow.type = "HTSeq - Counts")
+query <- GDCquery(project = "TARGET-AML",
+                  data.category = "Transcriptome Profiling",
+                  data.type = "Gene Expression Quantification",
+                  workflow.type = "HTSeq - Counts")
