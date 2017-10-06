1af2ef8c58a3d27a7c8df26c6ac9914269946b08
Scott Daniel <scottdaniel25@gmail.com>
2017-10-06 12:05:44 -0700

    weee

--- scripts/workers/interactive_tcga_bioLinks.R
+++ scripts/workers/interactive_tcga_bioLinks.R
@@ -1,11 +1,13 @@
 # source("https://bioconductor.org/biocLite.R")
 # biocLite("TCGAbiolinks")
 
+#working with R version 3.4.2 (2017-09-28)
+
 library(TCGAbiolinks)
 library(dplyr)
 library(DT)
 
-query <- GDCquery(project = "TARGET-AML",
+queryCOAD <- GDCquery(project = "TCGA-COAD",
                   data.category = "Transcriptome Profiling",
                   data.type = "Gene Expression Quantification",
                   workflow.type = "HTSeq - Counts")
