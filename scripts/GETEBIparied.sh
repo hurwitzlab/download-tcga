dd694156f47330e8dd5f5258be77643f039bf33b
Scott G. Daniel <scottdaniel25@gmail.com>
2017-12-08 11:36:20 -0700

    yay

---
+++ scripts/GETEBIparied.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+#PBS -W group_list=bhurwitz
+#PBS -q standard
+#PBS -l select=1:ncpus=1:mem=1gb
+#PBS -l walltime=72:00:00
+#PBS -l cput=72:00:00
+#PBS -M scottdaniel@email.arizona.edu
+#PBS -m ea
+#PBS -j oe
+#PBS -o pbs_logs/
+#PBS -e pbs_logs/
+
+export WD=$PBS_O_WORKDIR
+
+cd $WD
+
+mkdir -p pbs_logs
+
+if [ -e config.sh ]; then
+    source config.sh
+else
+    echo NO config file!
+    exit 12345
+fi
+
+if [ ! -d "$DL_DIR" ]; then
+    mkdir -p $DL_DIR
+fi
+
+cd $DL_PAIRED_DIR
+
+cut -f 10 $MASTERLIST | egrep ';' | tr ';' ' ' > $CART
+
+echo Downloading files in $CART
+
+if [ -e $WD/pbs_logs/download-wget.log ]; then
+    rm $WD/pbs_logs/download-wget.log
+fi
+
+for FILE in $(cat $CART); do
+    echo Downloading $FILE
+    time wget -nc -nd -r --no-parent \
+        $FILE &>> $WD/pbs_logs/download-wget.log
+done
+
--- scripts/config.sh
+++ scripts/config.sh
@@ -5,8 +5,10 @@ export PRJ_DIR="/rsgrps/bhurwitz/scottdaniel/download-ebi"
 #where the worker scripts are (PBS batch scripts and their python/perl workdogs)
 export WORKER_DIR="$SCRIPT_DIR/workers"
 
-export CART="files_to_get_list"
+export MASTERLIST="$PRJ_DIR/PRJEB12449.txt"
+export CART="$PRJ_DIR/files_to_get_list"
 
 #main download / working directory
 export DL_DIR="/rsgrps/bhurwitz/hurwitzlab/data/controlled_access/PRJEB12449"
+export DL_PAIRED_DIR="$DL_DIR/paired"
 
