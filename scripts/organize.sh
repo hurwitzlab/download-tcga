0947875bbb53c19412814e6daa514f8e4551e74d
Scott G. Daniel <scottdaniel25@gmail.com>
2018-01-08 17:57:01 -0700

    shorter time

--- scripts/organize.sh
+++ scripts/organize.sh
@@ -3,8 +3,8 @@
 #PBS -W group_list=bhurwitz
 #PBS -q standard
 #PBS -l select=1:ncpus=1:mem=1gb
-#PBS -l walltime=72:00:00
-#PBS -l cput=72:00:00
+#PBS -l walltime=4:00:00
+#PBS -l cput=4:00:00
 #PBS -M scottdaniel@email.arizona.edu
 #PBS -m ea
 #PBS -j oe
